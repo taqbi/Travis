5c6d1f17cd30e3faa7178fc2dc4b3107fcc4321b
taqbi <tawqeercse@gmail.com>
2018-10-03 15:59:24 +0530

    FIrst Commit

---
+++ Travis intro/src/RecursiveStringReplace.java
@@ -0,0 +1,29 @@
+
+/**
+ * A class that has a methond to recursively replaces characters in a String.
+ * 
+ * @author Charles Hoot
+ * @version 4.0
+ */
+public class RecursiveStringReplace
+{
+
+    /**
+     * replace - Replace all instances of one character by another.
+     * 
+     * @param  s   The string to do the replacement on.
+     * @param  from   The character to be replaced.
+     * @param  to   the character to change to.
+     * @return     A new string with the appropriate characters replaced.
+     */
+    public String replace(String s, char from, char to)
+    {
+        String result = null;
+
+        // IMPLEMENT THIS RECURSIVE METHOD
+
+        return result;
+    }
+
+
+}
